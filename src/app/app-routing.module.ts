2b4816bd47a8e247d5ee3c7810213407d89e6cbb
Matt Wicks <matt.wicks@gmail.com>
2024-01-15 13:32:01 +1000

    added company-edit component and routes for add/edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CompanyListComponent } from './company/company-list/company-list.component';
+import { CompanyEditComponent } from './company/company-edit/company-edit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'company/list', pathMatch: 'full' },
   { path: 'company/list', component: CompanyListComponent },
+  { path: 'company/add', component: CompanyEditComponent },
+  { path: 'company/edit/:id', component: CompanyEditComponent },
 ];
 
 @NgModule({
