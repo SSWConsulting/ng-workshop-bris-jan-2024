8bd519321f718fbc00e270410648660b4ea9ca92
Matt Wicks <matt.wicks@gmail.com>
2024-01-15 13:28:34 +1000

    added router outlet and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanyListComponent } from './company/company-list/company-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'company/list', pathMatch: 'full' },
+  { path: 'company/list', component: CompanyListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
