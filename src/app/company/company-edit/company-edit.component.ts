3d27759a8db075e506fef23d34424696a80bb383
Matt Wicks <matt.wicks@gmail.com>
2024-01-15 14:04:17 +1000

    company add - wired up reactive form

--- src/app/company/company-edit/company-edit.component.ts
+++ src/app/company/company-edit/company-edit.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { CompanyService } from '../company.service';
+import { Router } from '@angular/router';
+import { Company } from '../company';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'fbc-company-edit',
   templateUrl: './company-edit.component.html',
-  styleUrl: './company-edit.component.scss'
+  styleUrl: './company-edit.component.scss',
 })
 export class CompanyEditComponent {
+  // formGroup = new FormGroup({
+  //   name: new FormControl('', [Validators.required]),
+  //   email: new FormControl('', [Validators.required, Validators.email]),
+  //   phone: new FormControl('', [Validators.required]),
+  // });
+  // formGroup = this.fb.group({
+  //   name: this.fb.control('', [Validators.required]),
+  //   email: this.fb.control('', [Validators.required, Validators.email]),
+  //   phone: this.fb.control('', [Validators.required]),
+  // });
+  formGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    phone: ['', [Validators.required]],
+  });
 
+  constructor(
+    private readonly companyService: CompanyService,
+    private readonly fb: FormBuilder,
+    private readonly router: Router
+  ) {}
+
+  f() {
+    return this.formGroup.controls;
+  }
+
+  saveCompany() {
+    if (this.formGroup.valid) {
+      const company = this.formGroup.value as unknown as Company;
+      this.companyService
+        .addCompany(company)
+        .subscribe(() => this.router.navigate(['/company/list']));
+    }
+  }
 }
--- src/app/company/company.service.ts
+++ src/app/company/company.service.ts
@@ -18,6 +18,12 @@ export class CompanyService {
       .pipe(catchError(this.errorHandler<Company[]>));
   }
 
+  addCompany(company: Company): Observable<Company> {
+    return this.httpClient
+      .post<Company>(`${this.API_BASE}/company`, company)
+      .pipe(catchError(this.errorHandler<Company>));
+  }
+
   deleteCompany(companyId: number): Observable<Company> {
     return this.httpClient
       .delete<Company>(`${this.API_BASE}/company/${companyId}`)
