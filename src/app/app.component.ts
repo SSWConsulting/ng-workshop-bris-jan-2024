7d7f686c1e1ec3ae903644ba5a776c423d78001a
Matt Wicks <matt.wicks@gmail.com>
2024-01-15 15:19:31 +1000

    state management - use behaviour subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CompanyService } from './company/company.service';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'fbc-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'firebootcamp-crm';
+
+  companyCount$!: Observable<number>;
+
+  constructor(private readonly companyService: CompanyService) {}
+
+  ngOnInit(): void {
+    this.companyCount$ = this.companyService
+      .getCompanies()
+      .pipe(map((c) => c.length));
+  }
 }
--- src/app/company/company.service.ts
+++ src/app/company/company.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Company } from './company';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,12 +10,14 @@ import { catchError } from 'rxjs/operators';
 export class CompanyService {
   API_BASE = 'https://app-fbc-crm-api-prod.azurewebsites.net/api';
 
-  constructor(private readonly httpClient: HttpClient) {}
+  companies$ = new BehaviorSubject<Company[]>([]);
+
+  constructor(private readonly httpClient: HttpClient) {
+    this.loadCompanies();
+  }
 
   getCompanies(): Observable<Company[]> {
-    return this.httpClient
-      .get<Company[]>(`${this.API_BASE}/company`)
-      .pipe(catchError(this.errorHandler<Company[]>));
+    return this.companies$;
   }
 
   getCompany(companyId: number): Observable<Company> {
@@ -27,19 +29,35 @@ export class CompanyService {
   addCompany(company: Company): Observable<Company> {
     return this.httpClient
       .post<Company>(`${this.API_BASE}/company`, company)
-      .pipe(catchError(this.errorHandler<Company>));
+      .pipe(
+        catchError(this.errorHandler<Company>),
+        tap(() => this.loadCompanies())
+      );
   }
 
   updateCompany(company: Company): Observable<Company> {
     return this.httpClient
       .put<Company>(`${this.API_BASE}/company/${company.id}`, company)
-      .pipe(catchError(this.errorHandler<Company>));
+      .pipe(
+        catchError(this.errorHandler<Company>),
+        tap(() => this.loadCompanies())
+      );
   }
 
   deleteCompany(companyId: number): Observable<Company> {
     return this.httpClient
       .delete<Company>(`${this.API_BASE}/company/${companyId}`)
-      .pipe(catchError(this.errorHandler<Company>));
+      .pipe(
+        catchError(this.errorHandler<Company>),
+        tap(() => this.loadCompanies())
+      );
+  }
+
+  private loadCompanies(): void {
+    this.httpClient
+      .get<Company[]>(`${this.API_BASE}/company`)
+      .pipe(catchError(this.errorHandler<Company[]>))
+      .subscribe((c) => this.companies$.next(c));
   }
 
   private errorHandler<T>(error: Error): Observable<T> {
