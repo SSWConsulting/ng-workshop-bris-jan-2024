2e760e44e9c0aaa3485589ddc36c97a85af2b3cd
Matt Wicks <matt.wicks@gmail.com>
2024-01-15 14:16:25 +1000

    company edit component - add in todos for add company

--- src/app/company/company-edit/company-edit.component.ts
+++ src/app/company/company-edit/company-edit.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CompanyService } from '../company.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Company } from '../company';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'fbc-company-edit',
   templateUrl: './company-edit.component.html',
   styleUrl: './company-edit.component.scss',
 })
-export class CompanyEditComponent {
+export class CompanyEditComponent implements OnInit {
+  companyId!: number;
+  isNewCompany!: boolean;
+
   // formGroup = new FormGroup({
   //   name: new FormControl('', [Validators.required]),
   //   email: new FormControl('', [Validators.required, Validators.email]),
@@ -29,8 +37,19 @@ export class CompanyEditComponent {
   constructor(
     private readonly companyService: CompanyService,
     private readonly fb: FormBuilder,
+    private readonly activatedRoute: ActivatedRoute,
     private readonly router: Router
   ) {}
+
+  ngOnInit(): void {
+    this.companyId = +this.activatedRoute.snapshot.params['id'];
+    this.isNewCompany = !this.companyId;
+
+    if (!this.isNewCompany) {
+      // TODO:
+      // this.getCompany();
+    }
+  }
 
   f() {
     return this.formGroup.controls;
@@ -39,9 +58,14 @@ export class CompanyEditComponent {
   saveCompany() {
     if (this.formGroup.valid) {
       const company = this.formGroup.value as unknown as Company;
-      this.companyService
-        .addCompany(company)
-        .subscribe(() => this.router.navigate(['/company/list']));
+      if (this.isNewCompany) {
+        this.companyService
+          .addCompany(company)
+          .subscribe(() => this.router.navigate(['/company/list']));
+      } else {
+        // TODO:
+        // this.updateCompany()
+      }
     }
   }
 }
